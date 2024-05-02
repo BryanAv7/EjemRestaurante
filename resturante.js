7f29f977f9ce7341e73d43aa41933266ffded374
BryanAv7 <bavilag@est.ups.edu.ec>
2024-05-02 05:42:45 -0500

    Inicial

---
+++ resturante.js
@@ -0,0 +1,24 @@
+
+var totalComida = 2800 + 1300;
+var totalBebida = 900 + 650;
+
+var total = totalComida + totalBebida;
+console.log("El total de comida es:" + totalComida);
+console.log("El total de bebida es:" + totalBebida);
+console.log("El total de la cuenta es:" + total);
+
+console.log("------------------------------------");
+
+
+// CONDICIONES "IF", posible opcion V o F
+// CONDICION "ELSE", otra condicion
+
+var aprobe = false;
+
+if (aprobe == true) {
+console.log("Puedo salir de la U");
+
+}
+else {
+    console.log("Me quedo de año");
+}
